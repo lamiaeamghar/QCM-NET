0147c7b1e5b068214699839c62b6b86c2ec17dc2
Lamiae <lamiaeamgr@gmail.com>
2025-10-24 21:29:20 +0100

    Remove .nuxt build folder from tracking

--- .nuxt/middleware.js
+++
@@ -1,6 +0,0 @@
-const middleware = {}
-
-middleware['class'] = require('..\\middleware\\class.js')
-middleware['class'] = middleware['class'].default || middleware['class']
-
-export default middleware
